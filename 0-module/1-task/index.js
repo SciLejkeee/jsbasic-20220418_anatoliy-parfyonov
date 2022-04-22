8ad9fdb9b99fb3ab461881dbe9e3d6ea2465736c
SciLejkeee <103950841+SciLejkeee@users.noreply.github.com>
2022-04-22 19:30:23 +0500

    Update index.js

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sum(m, n) {
-  // ваш код...
+ return m+n;
 }
+
+let result = sum (1,1);
+alert ( result );
+
+function sum(a, b) {
+ return a+b;
+}
+
+let result = sum (2,5);
+alert ( result );
